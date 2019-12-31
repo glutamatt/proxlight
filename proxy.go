ea731a32fee835b675431a7064e201a9617ea647
Glutamatt <glutamatt@live.fr>
2019-12-31 20:07:19 +0100

    quick and dirty options , todo : test and clean and document

--- proxy.go
+++ proxy.go
@@ -7,6 +7,7 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"os"
+	"time"
 )
 
 func main() {
@@ -17,7 +18,8 @@ func main() {
 	check(nil, fmt.Sprintf("Proxiing %s through %s", target, listen))
 	proxy := httputil.NewSingleHostReverseProxy(target)
 	proxy.ErrorLog = log.New(os.Stdin, "[ERROR PROXY] ", log.LstdFlags)
-	check(http.ListenAndServe(listen, proxy), "http.ListenAndServe")
+
+	check(http.ListenAndServe(listen, enableCors(forgeHost(target.Host, throttle(10, proxy)))), "http.ListenAndServe")
 }
 
 func check(err error, step string) {
@@ -36,4 +38,30 @@ func parseArgs() (string, string, error) {
 		return "", "", fmt.Errorf("usage : ./proxy http://domain:port/path 0.0.0.0:1234")
 	}
 	return os.Args[1], os.Args[2], nil
+}
+
+func enableCors(handler http.Handler) http.HandlerFunc {
+	return http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
+		resp.Header().Set("Access-Control-Allow-Origin", "*")
+		handler.ServeHTTP(resp, req)
+	})
+}
+
+func forgeHost(hostname string, handler http.Handler) http.HandlerFunc {
+	log.Println("Forge request host with", hostname)
+	return http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
+		req.Host = hostname
+		req.URL.Host = hostname
+		req.Header.Set("Host", hostname)
+		handler.ServeHTTP(resp, req)
+	})
+}
+
+func throttle(reqPerSec int, handler http.Handler) http.HandlerFunc {
+	ticker := time.NewTicker(time.Second / time.Duration(reqPerSec))
+	log.Printf("Throttle %d req per sec", reqPerSec)
+	return http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
+		<-ticker.C
+		handler.ServeHTTP(resp, req)
+	})
 }
